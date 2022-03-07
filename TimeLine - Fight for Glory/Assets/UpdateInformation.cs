58fa09db6f0315ead73b757e90d84c5c92ea20c4
saladass419 <83832648+saladass419@users.noreply.github.com>
2022-03-07 18:40:43 +0100

    info ui

---
+++ TimeLine - Fight for Glory/Assets/UpdateInformation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UpdateInformation : MonoBehaviour
+{
+    private Image image;
+    private TextMeshProUGUI nameTxt;
+    private TextMeshProUGUI descText;
+    private void Start()
+    {
+        image = GetComponentsInChildren<Image>()[1];
+        nameTxt = GetComponentsInChildren<TextMeshProUGUI>()[0];
+        descText = GetComponentsInChildren<TextMeshProUGUI>()[1];
+        foreach (var item in FindObjectsOfType<CurrentSkillDataOnUI>())
+        {
+            item.onButtonClicked += UpdateInformationInUI;
+        }
+    }
+    private void UpdateInformationInUI(SkillData skill, int level)
+    {
+        image.sprite = skill.Sprite;
+        nameTxt.text = skill.SkillName;
+        descText.text = skill.Description;
+    }
+}
